0b287e5d17a420350440f6de284d2bf8d8a8d4c9
goldenchern <jiachernkch@gmail.com>
2024-04-06 17:48:17 +0800

    Update maze visualizer

--- maze_visualizer.py
+++ maze_visualizer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt                 # type: ignore
 from matplotlib.colors import ListedColormap    # type: ignore
+from copy import deepcopy
 
 
 '''
@@ -9,9 +10,9 @@ USAGE EXAMPLE
 from maze_visualizer import *
 
 # Visualize a blank maze
-show_maze(sample_maze)
+show_maze(sample_maze, (5, 6))
 
-# List storing valid path squares 
+# List storing valid cell coordinates
 path = [[6, 6], [6, 5], [6, 4], [5, 4], [5, 3], [5, 2], [5, 1], [4, 1], [3, 1], [2, 1]]
 
 # Visualize the searched maze
@@ -19,16 +20,6 @@ add_path(sample_maze, path)
 show_maze(sample_maze)
 
 '''
-
-
-# Define mapping of colors
-color_map = {
-    "white": 0,
-    "black": 1,
-    "yellow": 2,
-    "blue": 3,
-    "red": 4
-}
 
 
 # Edit these for your own maze design
@@ -47,26 +38,50 @@ blank_maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 
 
 sample_maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 4, 1, 0, 0, 0, 0, 1, 0, 0, 4, 1],
+               [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
                [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1],
                [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
                [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-               [1, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 1],
+               [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
                [1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
                [1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
                [1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1],
                [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
-               [1, 4, 1, 0, 0, 0, 0, 0, 0, 0, 4, 1],
+               [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 
     
 # Visualises the maze corresponding to grid passed in
-# Rejects incomplete mazes, i.e. mazes without all four colors
-def show_maze(grid):
+def show_maze(grid, start=None, path=None, show_corners=[1, 1, 1, 1]):
+    # Make a copy of the maze
+    grid_copy = deepcopy(grid)
+    
+    color_map = {
+        "white": 0,
+        "black": 1,
+        "yellow": 2,
+        "blue": 3,
+        "red": 4
+    }
+    
     # Input validation
-    if not valid_maze(grid):
-        raise ValueError("Maze is incomplete! Must have all four colors")
+#     if not valid_maze(grid):
+#         raise ValueError("Maze is incomplete! Must have all four colors")
+    
+    # Add path to the maze
+    if path:
+        grid_copy = add_path(grid_copy, path)
+        
+    # Add Pac-Man to the maze
+    if start:
+        grid_copy[start[0]][start[1]] = color_map["yellow"]
+    
+    # Add color for four corners
+    corners = [(1, 1), (1, len(grid[0]) - 2), (len(grid) - 2, 1), (len(grid) - 2, len(grid[0]) - 2)]
+    for i in range(4):
+        if show_corners[i]:
+            grid_copy[corners[i][0]][corners[i][1]] = color_map["red"]
     
     # Define the colors for specific values
     colors = color_map.keys()
@@ -78,7 +93,7 @@ def show_maze(grid):
     fig, ax = plt.subplots()
 
     # Draw the grid with the custom colormap
-    ax.imshow(grid, cmap=cmap, interpolation='nearest')
+    ax.imshow(grid_copy, cmap=cmap, interpolation='nearest')
 
     # Hide the axes labels
     ax.axis('off')
@@ -86,7 +101,8 @@ def show_maze(grid):
     # Display the plot
     plt.show()
 
-    
+
+# Helper functions
 # Accepts a list of coordinates of the path squares
 # Checks if all path squares are valid before adding them to the maze grid
 def add_path(grid, path):
@@ -99,7 +115,7 @@ def add_path(grid, path):
     return grid
         
 
-# Helper function to determine maze validity
+# Determines maze validity 
 def valid_maze(grid):
     yellow = red = False
     
